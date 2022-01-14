e80c740081e4d4d6463d99a3103b8387185becd7
P-Colin <colin.pallier@gmail.com>
2022-01-14 10:53:26 +0100

    feat : ajout de la classe PlateauQuantik

---
+++ PlateauQuantik.php
@@ -0,0 +1,51 @@
+<?php
+
+class PlateauQuantik
+{
+public const NBROWS = 4;
+public const NBCOLS = 4;
+public const NW = 0;
+public const NE = 1;
+public const SW = 2;
+public const SE = 3;
+
+protected Array $cases;
+
+public function __construct()
+{
+
+}
+
+public function getPiece(int $numRow, int $colNum):PieceQuantik
+{
+
+}
+public function setPiece(int $rowNum,int $colNum, PieceQuantik $p)
+{
+
+}
+public function getRow(int $numRow):Array
+{
+
+}
+public function getCol(int $numCol):Array
+{
+
+}
+
+public function getCorner(int $dir):Array
+{
+
+}
+
+public function __toString()
+{
+    // TODO: Implement __toString() method.
+}
+
+public static function getCornerFromCoord(int $rowNum, int $colNum):int
+{
+    
+}
+
+}
