030589f61e5ce53d52ad7f2ee62dd523c93c2139
Adzerty <adzerty76@outlook.fr>
2022-01-17 13:53:52 +0100

    Feat : ajout d'un script de test

---
+++ testHTMLMaker.php
@@ -0,0 +1,16 @@
+<?php
+include_once("HTMLmaker.php");
+include_once("PieceQuantik.php");
+include_once("ArrayPieceQuantik.php");
+include_once("PlateauQuantik.php");
+include_once("ActionQuantik.php");
+
+$html = new HTMLmaker();
+
+echo $html->getDebutHTML();
+
+$set_blanc = ArrayPieceQuantik::initPiecesBlanches();
+
+echo $html->getDivPiecesDisponibles($set_blanc);
+
+echo $html->getFinHTML();
