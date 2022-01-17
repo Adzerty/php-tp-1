f7765f857da15bcb012f76c5f26e9cc44fbf88c1
P-Colin <colin.pallier@gmail.com>
2022-01-17 13:32:58 +0100

    feat :  ajout classe HTMLmaker

---
+++ HTMLmaker.php
@@ -0,0 +1,17 @@
+<?php
+
+class HTMLmaker
+{
+    public function __construct(){
+
+    }
+
+    public function getDebutHTML():string{
+        $retour = "<html>";
+        $retour .= "\t"
+
+
+
+      return $retour;
+    }
+}
